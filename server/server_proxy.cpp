2e41c8746bd816c7a7150bccb2d440484e7a5adc
Fernando D S Lima <fernandodsl@gmail.com>
2015-11-11 21:03:53 -0300

    Quick commit

--- server/server_proxy.cpp
+++ server/server_proxy.cpp
@@ -14,11 +14,16 @@ using namespace std;
 
 static FileLockMap gFileMap;
 
+ordered_lock  gSlavesLock;
+static vector<pair<string, vector<string>> gSlaves; // slaves -> files mapping
+
 
 void HandleCreate(const Message &msg, Connection &con);
 void HandleRemove(const Message &msg, Connection &con);
 void HandleRead(const Message &msg, Connection &con);
 void HandleWrite(const Message &msg, Connection &con);
+
+void ManageSlaves(const bool &quit);
 
 
 void HandleOperations(const Message &msg) {
@@ -61,6 +66,9 @@ void Proxy(const bool &lead, const bool &quit) {
     return;
   }
 
+  thread slaves_thread(ManageSlaves, ref(quit));
+  slaves_thread.detach();
+
   while (!quit) {
     if (!con.HasMessage()) continue;
     auto msg = con.GetMessage();
@@ -88,13 +96,24 @@ void HandleCreate(const Message &msg, Connection &con) {
     msg.GetContent(op);
     cout << msg.sender() << " requested createop with args: " << op.file_name <<
             " and " << op.redundancy << endl;
-    auto client = msg.sender();
-    vector<string> slaves(op.redundancy, "huahua");
+    gSlavesLock.lock();
+    auto tmp_slv = gSlaves;
+    gSlavesLock.unlock();
+    if (op.redundancy > tmp_slv.size()) {
+      cout << "Requested redundancy cannot be met. Available: " << tmp_slv.size() << endl;
+      return;
+    }
 
+    auto slaves = vector(begin(tmp_slv), begin(tmp_slv) + op.redundancy);
+    auto client = msg.sender();
+
+    // TODO: Send request to slaves and once the request is confirmed persist information
     if (!gFileMap.CreateFile(op.file_name, slaves)) {
       cout << "Create Failed: File entry already exists." << endl;
       return;
     }
+
+
     cout << "Create Succeeded. Redundancy: " << slaves.size() << endl;
   } catch (...) {
     cout << "Invalid create request from " << msg.sender() << endl;
@@ -154,5 +173,43 @@ void HandleWrite(const Message &msg, Connection &con) {
     cout << "Write Succeeded." << endl;
    } catch (...) {
     cout << "Invalid write request from " << msg.sender() << endl;
+  }
+}
+
+void ManageSlaves(const bool &quit) {
+  bool err;
+  auto con = Connection::Connect(err, true);
+  if (err) {
+    cout << "Failed to connect with spread daemon." << endl;
+  }
+  if (!con.JoinGroup(kSlavesGroup)) {
+    cout << "Failed to join group: " << kSlavesGroup << endl;
+    return;
+  }
+
+  while (!quit) {
+    if (!con.HasMessage()) continue;
+    auto msg = con.GetMessage();
+
+    if (!msg.IsMembership()) {
+      cout << "Spurious message in slaves group." << endl;
+      continue;
+    }
+    auto slaves = msg.group();
+    slaves.erase(remove(begin(slaves), end(slaves), con.identifier()));
+
+    gSlavesLock.lock();
+    // remove dead ones
+    gSlaves.erase(std::remove_if(begin(gSlaves), end(gSlaves), [] (auto entry) {
+      return find(begin(slaves), end(slaves), entry.first) == end(slaves);
+    }), end(gSlaves));
+    // insert new ones
+    copy_if(begin(slaves), end(slaves), back_inserter(gSlaves), [] (auto slave) {
+      return find_if(begin(gSlaves), end(gSlaves), [] (auto entry) {
+        return entry.first == slave; }) == end(gSlaves);
+    });
+    sort(begin(gSlaves), end(gSlaves),
+         [] (auto a, auto b) {return a.second.size() < b.second.size();});
+    gSlavesLock.unlock();
   }
 }
---
+++ utils/ordered_lock.h
@@ -0,0 +1,30 @@
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+
+class ordered_lock {
+
+    std::queue<std::condition_variable>  cvar;
+    std::mutex                           cvar_lock;
+    bool                                 locked;
+public:
+    ordered_lock() : locked(false) {}
+    void lock() {
+        std::unique_lock<std::mutex> acquire(cvar_lock);
+        if (locked) {
+            cvar.emplace();
+            cvar.back().wait(acquire);
+        } else {
+            locked = true;
+        }
+    }
+    void unlock() {
+        std::unique_lock<std::mutex> acquire(cvar_lock);
+        if (cvar.empty()) {
+            locked = false;
+        } else {
+            cvar.front().notify_one();
+            cvar.pop();
+        }
+    }
+};
