d547578d7229831b375a599a3ef4871cb92bce84
Fernando D S Lima <fernandodsl@gmail.com>
2015-12-07 23:52:11 -0300

    Removing unneeded folder.

--- old/server/groups.h
+++
@@ -1,3 +0,0 @@
-const char* kProxyGroup = "SERVERS";
-const char* kLeadershipGroup = "LEADERSHIP";
-const char* kSlavesGroup = "SLAVES";
--- old/slave/fs.h
+++
@@ -1,8 +0,0 @@
-#include <string>
-
-bool InitStorage(std::string volume);
-bool Exists(std::string filename);
-bool CreateFile(std::string filename);
-bool RemoveFile(std::string filename);
-bool WriteFile(std::string filename, const std::string &data);
-bool ReadFile(std::string filename, std::string &data);
